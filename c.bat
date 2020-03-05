bd45661009697f2f9f56612be78ac32e80c29f08
Leonid Romanovsky <sirLeonidRomanovsky@gmail.com>
2020-03-06 01:44:53 +0300

    added premake5 to project

--- c.bat
+++ c.bat
@@ -1,4 +1,4 @@
 @echo off
-del main.exe
-clang++  src/*.cpp -o main.exe -Iskia_include -Iinclude -Llib -lSDL2 -lSDL2main -lskia -lUser32 -Xlinker -subsystem:windows
-main.exe > run.log
+del tumbleweed.exe
+premake5 gmake2
+make CXX=clang++ SHELL=cmd
---
+++ premake5.lua
@@ -0,0 +1,26 @@
+workspace "tumbleweed"
+    configurations { "Release", "Debug" }
+
+project "tumbleweed"
+    kind "WindowedApp"
+    language "C++"
+    files { "src/**.cpp" }
+    includedirs {
+        "skia_include",
+        "include"
+    }
+    libdirs { "lib" }
+    links {
+        "SDL2",
+        "SDL2main",
+        "skia",
+        "User32"
+    }
+    linkoptions { "-Xlinker -subsystem:windows" }
+    symbols "On"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+
+    filter "configurations:Release"
+        optimize "On"
