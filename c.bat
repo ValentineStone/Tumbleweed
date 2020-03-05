7d6b75bc731cba2e8e0b81176f4b14c44aecf495
Leonid Romanovsky <sirLeonidRomanovsky@gmail.com>
2020-03-05 14:53:10 +0300

    designated source folder

--- c.bat
+++ c.bat
@@ -1,4 +1,4 @@
 @echo off
 del main.exe
-clang++  main.cpp SDL.cpp -o main.exe -Iskia_include -Iinclude -Llib -lSDL2 -lSDL2main -lskia -lUser32 -Xlinker -subsystem:windows
+clang++  src/*.cpp -o main.exe -Iskia_include -Iinclude -Llib -lSDL2 -lSDL2main -lskia -lUser32 -Xlinker -subsystem:windows
 main.exe > run.log
